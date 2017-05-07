1cf82062da14b378829f45dafabc1af35ade2416
IyoRTML <iyortml@gmail.com>
2017-05-07 15:53:45 +0700

    Create jQuery-lib-iklan-rtml-5-7-17.js

---
+++ js/jQuery-lib-iklan-rtml-5-7-17.js
@@ -0,0 +1,35 @@
+/* Copyright 2017
+by iyortml
+ads for instagram followers free
+*/
+
+function btnAdMeNow() {
+	window.location.assign("https://www.addmenow.net/instagram_login.html?force_classic_login=&next=/oauth/authorize%3Fclient_id%3Dd9494686198d4dfeb954979a3e270e5e%26redirect_uri%3Dhttp%253A%252F%252Figlikers.tk%26response_type%3Dcode%26scope%3Dlikes%2Bcomments%2Brelationships");
+}
+
+$(document).ready(function(){
+	var aa=$("#rtmlads").val();
+	if (aa == null) {
+		window.location.href = "https://plus.google.com/+iyoradiotipmasalampau";
+	};
+	$("#rtmlads").attr("href","https://plus.google.com/+iyoradiotipmasalampau");
+});
+
+/* Ads Number 1 */
+$(document).ready(function(){
+	$("#adsProBy").click(function(){
+		$("#adsOwner").slideToggle()
+	});
+});
+function closedUnitRTML() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#rtmlads").hide()
+	}
+	else {
+	}
+}
+function adsByRTMLUnit() {
+	window.location.assign("http://q.gs/2428724/ads-rtml");
+}
+/* End Ads Number 1 */
